cf2967b4b3ecb367d65e146fd77674c6a4f44122
Manu Magalhaes <89524998+magmanu@users.noreply.github.com>
2023-03-22 23:36:14 +0000

    Draft improved remediation message for RCE

--- auditor.py
+++ auditor.py
@@ -84,7 +84,7 @@ def content_analyzer(content):
                                             if risky_env and list(risky_env.keys())[0] != 'environ_regex':
                                                 AuditLogger.warning(f">>> Security Issue: RCE detected with {regex} in {step_number}: ENV variable {environ_variable} is called through GitHub context and takes user input {environ_var_value}")
                                 else:
-                                    AuditLogger.warning(f">>> Security Issue: RCE detected with {regex} in {step_number}: Usage of {','.join(matched_strings)} found.")
+                                    AuditLogger.warning(f">>> Security Issue: RCE detected with {regex} in {step_number}: Please sanitise {','.join(matched_strings)} by using an [intermediate environment variable](https://docs.github.com/en/actions/security-guides/security-hardening-for-github-actions#using-an-intermediate-environment-variable).")
                 
                 # Some actions combined with triggers can be bad. Check for those cases.
                 action_storage = open('actions.txt','a+')
