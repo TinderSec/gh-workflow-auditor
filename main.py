f0f85706509b3e074f9990d38a2bf8c6a3c5274c
Manu Magalhaes <89524998+magmanu@users.noreply.github.com>
2023-03-23 01:04:05 +0000

    Officially moving to os.env instead of argparse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import argparse
+import os
 
 # Local imports
 from auditor import content_analyzer
@@ -9,8 +9,7 @@ from lib.logger import AuditLogger
 
 """
 Input:
-   repo_dict - dictionary defining repo information
-   scan_folder - Location where the repo is cloned
+    repo_dict - dictionary defining repo information
 Output:
     scan result (if any) in scan.log file.
 Summary:
@@ -26,19 +25,10 @@ def repo_analysis(repo_workflow):
         content_analyzer(content=workflow_content) # will print out security issues
 
 def main():
-    # Supporting user provided arguments: type, and scan target.
-    # parser = argparse.ArgumentParser(description='Identify vulnerabilities in GitHub Actions workflow')
-    # parser.add_argument('--type',choices=['repo','org','user'],
-    #                     help='Type of entity that is being scanned.')
-    # parser.add_argument('input',help='Org, user or repo name (owner/name)')
-    # args = parser.parse_args()
-    
     gh = GHWrapper()
-    
-    # target_type = args.type #repo, org, or user
-    # target_input = args.input #can be repo url, or a username for org/user
-    target_type = "org" #repo, org, or user
-    target_input = "actions" #can be repo url, or a username for org/user
+
+    target_type = os.environ.get('TARGET_TYPE',None) #repo, org, or user
+    target_input = os.environ.get('TARGET_INPUT',None) #can be repo url, or a username for org/user
     
     if target_type == 'repo':
         repos = gh.get_single_repo(repo_name=target_input)
